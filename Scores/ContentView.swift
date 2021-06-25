c5696ca3788e7881341554ab3f17f6becc9db404
Jackson <jacksoncontreras4@gmail.com>
2021-06-25 14:58:00 -0700

    first commit

---
+++ Scores/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scores
+//
+//  Created by Jackson Contreras on 6/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            
+            .padding()
+        Text("LOOK HERE!").padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scores/ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresApp.swift
+//  Scores
+//
+//  Created by Jackson Contreras on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
